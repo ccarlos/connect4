1c009ee74ed1596273c3394d18bc849d29b667e3
Cesar Carlos <cesar.g.carlos@gmail.com>
2012-02-20 19:00:46 -0800

    [Add] Various methods for the GameBoard class.
    * add_piece, clear, set_board, column_full, column_empty, legal_move,
      is_full, rm_piece.

--- connect_four.py
+++ connect_four.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 
+
+# TODO: Replace.
+EMPTY_SLOT = ' '
+
+
 class Player:
     def __init__(self, name, score, game_piece):
         self.name = name
-        self.score = score 
+        self.score = score
         self.game_piece = game_piece
+
 
 class GamePiece:
     def __init__(self, piece_char, piece_color):
         self.piece_char = piece_char  # An 'x' or 'o'
         self.piece_color = piece_color
+
 
 class GameBoard:
     def __init__(self, column_size, row_size):
@@ -41,16 +48,79 @@ class GameBoard:
         for i in range(self.column_size):
             board_str += ' ' + str(i % 10)
         board_str += '\n'
-        
+
         return board_str
 
     def add_piece(self, column, game_piece):
-        pass
-        # Find the column and insert game_piece. If the lowest slot is taken,
-        # go up one level and check again. Do this until the piece is placed.
-        # We must check if the column is full prior to placing the piece.
+        """Add a game piece to game board, given a column and game_piece."""
+        for row in reversed(range(self.row_size)):
+            # Find first row with an empty slot.
+            if self.data[row][column] == ' ':
+                self.data[row][column] = game_piece
+                break
 
-        # After successfully placing piece make a check if the player has won.
+    def clear(self):
+        """Clears the gameboard of any game pieces intact."""
+        for row in range(self.row_size):
+            for col in range(self.column_size):
+                self.data[row][col] = ' '
 
-game = GameBoard(7, 6)
+    def set_board(self, move_string):
+        """Prefill a game board given a string of integers."""
+        game_piece = 'X'
+        for ch in move_string:
+            column = int(ch)
+            if 0 <= column <= self.column_size:
+                self.add_piece(column, game_piece)
+
+            game_piece = 'O' if game_piece == 'X' else 'X'
+
+    def column_full(self, column):
+        """A column is scanned to determine if there is room available."""
+        for row in reversed(range(self.row_size)):
+            if self.data[row][column] == ' ':
+                return False
+        return True
+
+    def column_empty(self, column):
+        """Is the column empty?"""
+        # Check the lowest row to determined if a game piece exists.
+        if self.data[self.row_size - 1][column] == ' ':
+            return True
+        return False
+
+    def legal_move(self, column):
+        """A move is legal if it has a valid column and has room available."""
+        if column < 0 or column > self.column_size:
+            return False
+        if self.column_full(column):
+            return False
+        return True
+
+    def is_full(self):
+        """Is the board full of game pieces?"""
+        for col in range(self.column_size):
+            if self.legal_move(col):
+                return False
+        return True
+
+    def rm_piece(self, column):
+        """Removes a game piece given a specified column."""
+        if self.column_empty(column):
+            pass
+        else:
+            for row in range(self.row_size):
+                if self.data[row][column] != ' ':
+                    self.data[row][column] = ' '
+                    break
+
+
+# Debug purposes.
+game = GameBoard(2, 2)
+game.set_board('0011')
+game.rm_piece(1)
+game.rm_piece(1)
+game.rm_piece(1)
+game.rm_piece(0)
+
 print game
